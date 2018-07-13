548ac8d6c7af4aea2ac15e9ca96d80e5ec3de6c0
Peter Örneholm <peter@orneholm.com>
2018-07-13 21:38:34 +0200

    Remove constant for personal identity scope as it's related to identity server

--- src/ActiveLogin.Authentication.BankId.AspNetCore/BankIdAuthenticationConstants.cs
+++ src/ActiveLogin.Authentication.BankId.AspNetCore/BankIdAuthenticationConstants.cs
@@ -7,7 +7,5 @@
         internal const int MinimumRefreshIntervalMs = 1000;
 
         internal const string InvalidReturnUrlErrorMessage = "Invalid returnUrl. Needs to be a local url.";
-
-        public const string PersonalIdentityNumberScopeName = "personalidentitynumber";
     }
 }
