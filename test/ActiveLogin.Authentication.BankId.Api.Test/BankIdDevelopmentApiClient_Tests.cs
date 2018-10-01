12b53fa159201e8656559318a671d748072784c0
Fredrik Lundin <fredrik.lundin@activesolution.se>
2018-10-01 23:57:48 +0200

    Allow multiple logins after one another in dev mode

---
+++ test/ActiveLogin.Authentication.BankId.Api.Test/BankIdDevelopmentApiClient_Tests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using ActiveLogin.Authentication.BankId.Api.Models;
+using Xunit;
+
+namespace ActiveLogin.Authentication.BankId.Api.Test
+{
+    public class BankIdDevelopmentApiClient_Tests
+    {
+        private readonly BankIdDevelopmentApiClient _bankIdClient;
+
+        public BankIdDevelopmentApiClient_Tests()
+        {
+            _bankIdClient = new BankIdDevelopmentApiClient
+            {
+                Delay = TimeSpan.Zero
+            };
+        }
+
+        [Fact]
+        public async void StartingTwoAuthenticationFlows__AtTheSameTime__ShouldThrow()
+        {
+            await _bankIdClient.AuthAsync(new AuthRequest("1.1.1.1", "199908072391"));
+            await Assert.ThrowsAsync<BankIdApiException>(() => _bankIdClient.AuthAsync(new AuthRequest("1.1.1.2", "199908072391")));
+        }
+
+        [Fact]
+        public async void StartingTwoAuthenticationFlows__AfterOneAnother__ShouldBeAllowed()
+        {
+            AuthResponse authResponse = await _bankIdClient.AuthAsync(new AuthRequest("1.1.1.1", "199908072391"));
+            CollectResponse response;
+            do
+            {
+                response = await _bankIdClient.CollectAsync(new CollectRequest(authResponse.OrderRef));
+            } while (response.Status != CollectStatus.Complete);
+
+            response = null;
+
+            authResponse = await _bankIdClient.AuthAsync(new AuthRequest("1.1.1.2", "199908072391"));
+            do
+            {
+                response = await _bankIdClient.CollectAsync(new CollectRequest(authResponse.OrderRef));
+            } while (response.Status != CollectStatus.Complete);
+
+            Assert.True(true, "Did not throw");
+        }
+    }
+}
