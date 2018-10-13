3697fe3b198758e418a752deb94f486ae7d794b4
Peter Örneholm <peter@orneholm.com>
2018-10-13 21:34:55 +0200

    Remove unused usings

--- samples/IdentityServerSample/Startup.cs
+++ samples/IdentityServerSample/Startup.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.IO;
 using ActiveLogin.Authentication.BankId.AspNetCore;
-using ActiveLogin.Authentication.BankId.AspNetCore.Azure;
 using ActiveLogin.Authentication.GrandId.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.CookiePolicy;
