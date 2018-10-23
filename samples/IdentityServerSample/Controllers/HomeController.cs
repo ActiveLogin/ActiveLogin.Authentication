51ca9c26d1cb799e7ff4ced1d8da1a7c9aa00c44
Peter Örneholm <peter@orneholm.com>
2018-10-23 22:09:52 +0200

    Update design for samples

--- samples/IdentityServerSample/Controllers/HomeController.cs
+++ samples/IdentityServerSample/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using IdentityServerSample.Models;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using IdentityServerSample.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,10 +12,22 @@ namespace IdentityServerSample.Controllers
         [Authorize]
         public IActionResult Index()
         {
+            var claims = User.Claims.ToList();
             return View(new HomeIndexViewModel()
             {
-                Claims = User.Claims
+                Name = GetClaimValue(claims, "name"),
+                GivenName = GetClaimValue(claims, "given_name"),
+                FamilyName = GetClaimValue(claims, "family_name"),
+                SwedishPersonalIdentityNumber = GetClaimValue(claims, "swedish_personal_identity_number"),
+                Birthdate = GetClaimValue(claims, "birthdate"),
+                Gender = GetClaimValue(claims, "gender"),
+                Claims = claims
             });
+        }
+
+        private string GetClaimValue(IEnumerable<Claim> claims, string type, string fallback = "-")
+        {
+            return claims.FirstOrDefault(x => x.Type == type)?.Value ?? fallback;
         }
     }
 }
--- samples/IdentityServerSample/Models/HomeIndexViewModel.cs
+++ samples/IdentityServerSample/Models/HomeIndexViewModel.cs
@@ -5,6 +5,13 @@ namespace IdentityServerSample.Models
 {
     public class HomeIndexViewModel
     {
+        public string GivenName { get; set; }
+        public string FamilyName { get; set; }
+        public string Name { get; set; }
+        public string SwedishPersonalIdentityNumber { get; set; }
+        public string Birthdate { get; set; }
+        public string Gender { get; set; }
+
         public IEnumerable<Claim> Claims { get; set; }
     }
 }
