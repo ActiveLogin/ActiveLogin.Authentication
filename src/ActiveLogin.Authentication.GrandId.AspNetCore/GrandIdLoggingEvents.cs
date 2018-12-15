68b2ce15a3c47a26ab38bfc02e4492e311baf573
Peter Örneholm <peter@orneholm.com>
2018-12-15 10:19:08 +0100

    Update comment

--- src/ActiveLogin.Authentication.GrandId.AspNetCore/GrandIdLoggingEvents.cs
+++ src/ActiveLogin.Authentication.GrandId.AspNetCore/GrandIdLoggingEvents.cs
@@ -4,11 +4,11 @@ namespace ActiveLogin.Authentication.GrandId.AspNetCore
 {
     internal static class GrandIdLoggingEvents
     {
-        // GrandId API - Auth
+        // GrandId - BankId - Federated Login
         public static readonly EventId GrandIdBankIdFederatedLoginSuccess = new EventId(1_1_1, nameof(GrandIdBankIdFederatedLoginSuccess));
         public static readonly EventId GrandIdBankIdFederatedLoginHardFailure = new EventId(1_1_2, nameof(GrandIdBankIdFederatedLoginHardFailure));
 
-        // GrandId API - Get session
+        // GrandId - BankId - Get session
         public static readonly EventId GrandIdBankIdGetSessionSuccess = new EventId(2_1_1, nameof(GrandIdBankIdGetSessionSuccess));
         public static readonly EventId GrandIdBankIdGetSessionHardFailure = new EventId(2_1_2, nameof(GrandIdBankIdGetSessionHardFailure));
     }
