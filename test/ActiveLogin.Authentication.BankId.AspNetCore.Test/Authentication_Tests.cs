a781458b3e34b5ec866af0a7e42e5e6b07a2b028
Peter Örneholm <peter@orneholm.com>
2019-01-23 18:05:57 +0100

    Update packages

--- test/ActiveLogin.Authentication.BankId.AspNetCore.Test/Authentication_Tests.cs
+++ test/ActiveLogin.Authentication.BankId.AspNetCore.Test/Authentication_Tests.cs
@@ -1,13 +1,16 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.TestHost;
 using Xunit;
 
 namespace ActiveLogin.Authentication.BankId.AspNetCore.Test
 {
     public class Authentication_Tests
     {
-        public Authentication_Tests()
-        {
-        }
+
 
         [Fact]
         public async Task Authentication_UI_Should_Be_Accessible_Even_When_Site_Uses_Auth()
