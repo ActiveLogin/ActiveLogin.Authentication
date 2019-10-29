5304853e9ff339f541d50f432d2faf74158b16e1
Daniel Kvist <daniel@danielkvist.net>
2019-10-29 12:32:47 +0100

    Fix broken test after merge

--- test/ActiveLogin.Authentication.BankId.AspNetCore.Test/BankId_Authentication_Tests.cs
+++ test/ActiveLogin.Authentication.BankId.AspNetCore.Test/BankId_Authentication_Tests.cs
@@ -167,7 +167,7 @@ namespace ActiveLogin.Authentication.BankId.AspNetCore.Test
 
             var content = await transaction.Content.ReadAsStringAsync();
             Assert.Contains("<form id=\"bankIdLoginForm\">", content);
-            Assert.Contains("<div id=\"bankIdLoginStatus\"", content);
+            Assert.Contains("<div class=\"card\" id=\"bankIdLoginStatus\" style=\"display: block;\">", content);
             Assert.Contains("<img src=\"\" alt=\"QR Code for BankID\" class=\"qr-code-image\" style=\"display: none;\" />", content);
         }
 
