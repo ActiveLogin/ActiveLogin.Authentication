2c9377918349ae694c380baf128a08c5270d4667
Peter Örneholm <peter@orneholm.com>
2019-12-04 09:46:00 +0100

    Fix wrong test

--- test/ActiveLogin.Authentication.BankId.AspNetCore.Test/UserMessage/BankIdSupportedDeviceDetector_Detect_Tests.cs
+++ test/ActiveLogin.Authentication.BankId.AspNetCore.Test/UserMessage/BankIdSupportedDeviceDetector_Detect_Tests.cs
@@ -144,7 +144,7 @@ namespace ActiveLogin.Authentication.BankId.AspNetCore.Test.UserMessage
             var userAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Mobile/15E148 Safari/604.1";
             var detectedDevice = _bankIdSupportedDeviceDetector.Detect(userAgent);
 
-            Assert.Equal(BankIdSupportedDeviceType.Desktop, detectedDevice.DeviceType);
+            Assert.Equal(BankIdSupportedDeviceType.Mobile, detectedDevice.DeviceType);
             Assert.Equal(BankIdSupportedDeviceOs.Windows, detectedDevice.DeviceOs);
             Assert.Equal(BankIdSupportedDeviceBrowser.Chrome, detectedDevice.DeviceBrowser);
         }
