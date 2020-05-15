f9db37efdb46a2da387b8773cf06b1c48381fd56
Elin Ohlsson <ElinO@activesolution.se>
2020-05-15 13:53:09 +0200

    changed comment.

--- src/ActiveLogin.Authentication.BankId.AspNetCore/Areas/BankIdAuthentication/Controllers/BankIdApiController.cs
+++ src/ActiveLogin.Authentication.BankId.AspNetCore/Areas/BankIdAuthentication/Controllers/BankIdApiController.cs
@@ -149,7 +149,7 @@ namespace ActiveLogin.Authentication.BankId.AspNetCore.Areas.BankIdAuthenticatio
         private static bool BrowserMightNotAutoLaunch(BankIdSupportedDevice detectedDevice)
         {
             // Some Android browsers might have issues launching a third party scheme (BankID) if there is no user interaction.
-            // Android version > 6 supports app links (https://app.bankid.com/)
+            // Android version > 6 supports app links (https://app.bankid.com/).
             return detectedDevice.DeviceOs == BankIdSupportedDeviceOs.Android && detectedDevice.DeviceOsVersion.MajorVersion < 6;
         }
 
