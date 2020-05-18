d0c1b60157c5725d9cb1dc528e55636f490ba903
Peter Örneholm <peter@orneholm.com>
2020-05-18 11:47:51 +0200

    Rename device properties (#255)
    * AMake clear it's Opera Mini
    
    * Rename preoprties for application insights
    
    * Add some missing clas documentation

--- src/ActiveLogin.Authentication.BankId.AspNetCore/SupportedDevice/BankIdSupportedDevice.cs
+++ src/ActiveLogin.Authentication.BankId.AspNetCore/SupportedDevice/BankIdSupportedDevice.cs
@@ -1,5 +1,8 @@
 namespace ActiveLogin.Authentication.BankId.AspNetCore.SupportedDevice
 {
+    /// <summary>
+    /// Information about the device that is supported by Active Login.
+    /// </summary>
     public class BankIdSupportedDevice
     {
         public BankIdSupportedDevice(BankIdSupportedDeviceType deviceType, BankIdSupportedDeviceOs deviceOs, BankIdSupportedDeviceBrowser deviceBrowser, BankIdSupportedDeviceOsVersion deviceOsVersion)
@@ -10,9 +13,24 @@ namespace ActiveLogin.Authentication.BankId.AspNetCore.SupportedDevice
             DeviceOsVersion = deviceOsVersion;
         }
 
+        /// <summary>
+        /// Device type (desktop, mobile)
+        /// </summary>
         public BankIdSupportedDeviceType DeviceType { get; }
+
+        /// <summary>
+        /// Device OS (Windows, iOS, Android etc)
+        /// </summary>
         public BankIdSupportedDeviceOs DeviceOs { get; }
+
+        /// <summary>
+        /// Device OS version (9.0.0, 1.0 etc).
+        /// </summary>
         public BankIdSupportedDeviceOsVersion DeviceOsVersion { get; }
+
+        /// <summary>
+        /// Device browser (Chrome, Firefox, Safari, Edge etc)
+        /// </summary>
         public BankIdSupportedDeviceBrowser DeviceBrowser { get; }
     }
 }
--- src/ActiveLogin.Authentication.BankId.AspNetCore/SupportedDevice/IBankIdSupportedDeviceDetector.cs
+++ src/ActiveLogin.Authentication.BankId.AspNetCore/SupportedDevice/IBankIdSupportedDeviceDetector.cs
@@ -1,10 +1,15 @@
-﻿namespace ActiveLogin.Authentication.BankId.AspNetCore.SupportedDevice
+namespace ActiveLogin.Authentication.BankId.AspNetCore.SupportedDevice
 {
     /// <summary>
     /// Detect if the device requesting is one of the supported platforms for BankID.
     /// </summary>
     public interface IBankIdSupportedDeviceDetector
     {
+        /// <summary>
+        /// Detect the device.
+        /// </summary>
+        /// <param name="userAgent">The user agent string to extract device information from.</param>
+        /// <returns></returns>
         BankIdSupportedDevice Detect(string userAgent);
     }
 }
