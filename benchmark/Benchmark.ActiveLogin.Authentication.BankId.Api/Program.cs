28468be409ee71cb223f085f100a5d8cf9b536e5
Nikolay Krondev <nikolay.krondev@live.com>
2020-08-07 12:51:45 +0200

    Benchmark for BankIdApiClient

---
+++ benchmark/Benchmark.ActiveLogin.Authentication.BankId.Api/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace ActiveLogin.Authentication.BankId.Api
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<BankIdApiClientBenchmark>();
+        }
+    }
+}
