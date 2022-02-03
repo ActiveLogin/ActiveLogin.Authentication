5cd9639874c8b8d455298965a897c04a7eb90d2a
Peter Örneholm <peter@orneholm.com>
2022-02-03 15:54:23 +0100

    Update URL:s to v5.1

--- src/ActiveLogin.Authentication.BankId.Api/BankIdUrls.cs
+++ src/ActiveLogin.Authentication.BankId.Api/BankIdUrls.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace ActiveLogin.Authentication.BankId.Api
 {
@@ -10,11 +10,11 @@ namespace ActiveLogin.Authentication.BankId.Api
         /// <summary>
         /// Base url for production API. Needs to be used in conjunction with a production certificate.
         /// </summary>
-        public static readonly Uri ProductionApiBaseUrl = new Uri("https://appapi2.bankid.com/rp/v5/");
+        public static readonly Uri ProductionApiBaseUrl = new Uri("https://appapi2.bankid.com/rp/v5.1/");
 
         /// <summary>
         /// Base url for test API. Needs to be used in conjunction with the test certificate.
         /// </summary>
-        public static readonly Uri TestApiBaseUrl = new Uri("https://appapi2.test.bankid.com/rp/v5/");
+        public static readonly Uri TestApiBaseUrl = new Uri("https://appapi2.test.bankid.com/rp/v5.1/");
     }
 }
