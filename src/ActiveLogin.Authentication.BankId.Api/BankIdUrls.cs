99f247c1bf42e6fda38bb368a0afbf72971ae279
Tobias Lolax <tobias.lolax@gmail.com>
2022-04-29 14:34:27 +0200

    Bump BankID API version to 5.1 in events (#359)
    * bump bankid api version to 5.1 used in the context
    
    Signed-off-by: Tobias Lolax <tobias.lolax@gmail.com>
    
    * consolidate bankid version used
    
    Signed-off-by: Tobias Lolax <tobias.lolax@gmail.com>

--- src/ActiveLogin.Authentication.BankId.Api/BankIdUrls.cs
+++ src/ActiveLogin.Authentication.BankId.Api/BankIdUrls.cs
@@ -7,14 +7,16 @@ namespace ActiveLogin.Authentication.BankId.Api
     /// </summary>
     public static class BankIdUrls
     {
+        public const string BankIdApiVersion = "5.1";
+
         /// <summary>
         /// Base url for production API. Needs to be used in conjunction with a production certificate.
         /// </summary>
-        public static readonly Uri ProductionApiBaseUrl = new Uri("https://appapi2.bankid.com/rp/v5.1/");
+        public static readonly Uri ProductionApiBaseUrl = new Uri($"https://appapi2.bankid.com/rp/v{BankIdApiVersion}/");
 
         /// <summary>
         /// Base url for test API. Needs to be used in conjunction with the test certificate.
         /// </summary>
-        public static readonly Uri TestApiBaseUrl = new Uri("https://appapi2.test.bankid.com/rp/v5.1/");
+        public static readonly Uri TestApiBaseUrl = new Uri($"https://appapi2.test.bankid.com/rp/v{BankIdApiVersion}/");
     }
 }
