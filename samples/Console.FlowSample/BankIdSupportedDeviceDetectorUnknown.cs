698686fe3eaa3a717b8f9f0ccccc3ddbadd6766c
Peter Örneholm <peter@orneholm.com>
2022-08-22 16:37:13 +0200

    Remove console sample

--- samples/Console.FlowSample/BankIdSupportedDeviceDetectorUnknown.cs
+++
@@ -1,9 +0,0 @@
-using ActiveLogin.Authentication.BankId.Core.SupportedDevice;
-
-public class BankIdSupportedDeviceDetectorUnknown : IBankIdSupportedDeviceDetector
-{
-    public BankIdSupportedDevice Detect()
-    {
-        return BankIdSupportedDevice.Unknown;
-    }
-}
