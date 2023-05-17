21c138b512bc9641df794a57b842dd8ff9171c0a
Peter Örneholm <peter@orneholm.com>
2023-05-17 22:05:45 +0200

    Remove grandid

--- src/ActiveLogin.Authentication.GrandId.AspNetCore/GrandIdConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ActiveLogin.Authentication.GrandId.AspNetCore;
-
-public static class GrandIdConstants
-{
-    internal const string ProductName = "ActiveLogin-GrandId-AspNetCore";
-
-    public const string AuthenticationPropertyItemSwedishPersonalIdentityNumber = "swedishPersonalIdentityNumber";
-}
--- src/ActiveLogin.Authentication.GrandId.AspNetCore/IGrandIdEnvironmentConfiguration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ActiveLogin.Authentication.GrandId.AspNetCore;
-
-public interface IGrandIdEnvironmentConfiguration
-{
-    string? ApiKey { get; set; }
-    string? BankIdServiceKey { get; set; }
-}
