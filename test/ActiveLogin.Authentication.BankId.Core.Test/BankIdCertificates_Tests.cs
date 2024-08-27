ef25886727481c22457826c91caecfae17a0a91b
Elin Fokine <ElinO@activesolution.se>
2024-08-27 14:54:16 +0200

    Do not test expiration for pem certificate since not a supported format on mac os.

--- test/ActiveLogin.Authentication.BankId.Core.Test/BankIdCertificates_Tests.cs
+++ test/ActiveLogin.Authentication.BankId.Core.Test/BankIdCertificates_Tests.cs
@@ -19,16 +19,6 @@ public class BankIdCertificates_Tests
     }
 
     [Fact]
-    public void BankIdApiClientCertificateTestPem_Should_Not_Be_Expired()
-    {
-        var cert = BankIdCertificates.GetBankIdApiClientCertificateTest(Certificate.TestCertificateFormat.PEM);
-        var certExpires = cert.NotAfter;
-        var now = DateTime.UtcNow;
-
-        Assert.True(now <= certExpires.Add(TimeBeforeExpire));
-    }
-
-    [Fact]
     public void BankIdApiClientCertificateTestPfx_Should_Not_Be_Expired()
     {
         var cert = BankIdCertificates.GetBankIdApiClientCertificateTest(Certificate.TestCertificateFormat.PFX);
