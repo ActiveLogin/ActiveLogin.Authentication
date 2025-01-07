8f6593a57ddd8b51ef377f5361434465c0d4ec1a
Peter Stålbrand <peter.stalbrand@activesolution.se>
2025-01-07 15:23:14 +0100

    Add BankIdApiExceptionJsonConverter to BankIdDebugEventListener  #270

--- src/ActiveLogin.Authentication.BankId.Core/Events/Infrastructure/BankIdDebugEventListener.cs
+++ src/ActiveLogin.Authentication.BankId.Core/Events/Infrastructure/BankIdDebugEventListener.cs
@@ -1,6 +1,8 @@
+using System.Reflection;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
+using ActiveLogin.Authentication.BankId.Api;
 using ActiveLogin.Identity.Swedish;
 
 using Microsoft.Extensions.Logging;
@@ -24,6 +26,7 @@ public class BankIdDebugEventListener : IBankIdEventListener
 
         _serializerOptions.Converters.Add(new JsonStringEnumConverter());
         _serializerOptions.Converters.Add(new PersonalIdentityNumberJsonConverter());
+        _serializerOptions.Converters.Add(new BankIdApiExceptionJsonConverter());
     }
 
     public Task HandleAsync(BankIdEvent bankIdEvent)
@@ -61,5 +64,44 @@ public class BankIdDebugEventListener : IBankIdEventListener
             PersonalIdentityNumber personalIdentityNumberValue,
             JsonSerializerOptions options) =>
             writer.WriteStringValue(personalIdentityNumberValue.To12DigitString());
+    }
+
+    // This converter is used to serialize BankIdApiException to JSON.
+    // It serializes all properties of the exception, except for properties
+    // of type "Type" and properties of types in the System.Reflection namespace.
+    // Otherwise, this will fail when serialization and deserialization
+    // with "'System.Reflection.MethodBase' instances are not supported. Path: $.TargetSite."
+    private class BankIdApiExceptionJsonConverter : JsonConverter<BankIdApiException>
+    {
+        public override BankIdApiException Read(
+            ref Utf8JsonReader reader,
+            Type typeToConvert,
+            JsonSerializerOptions options) =>
+            throw new NotImplementedException();
+        public override void Write(
+            Utf8JsonWriter writer,
+            BankIdApiException exception,
+            JsonSerializerOptions options)
+        {
+            writer.WriteStartObject();
+            var exceptionType = exception.GetType();
+            writer.WriteString("ClassName", exceptionType.FullName);
+            var properties = exceptionType.GetProperties()
+                .Where(e => e.PropertyType != typeof(Type))
+                .Where(e => e.PropertyType.Namespace != typeof(MemberInfo).Namespace)
+                .ToList();
+            foreach (var property in properties)
+            {
+                var propertyValue = property.GetValue(exception, null);
+                if (options.DefaultIgnoreCondition == JsonIgnoreCondition.WhenWritingNull && propertyValue == null)
+                {
+                    continue;
+                }
+
+                writer.WritePropertyName(property.Name);
+                JsonSerializer.Serialize(writer, propertyValue, property.PropertyType, options);
+            }
+            writer.WriteEndObject();
+        }
     }
 }
