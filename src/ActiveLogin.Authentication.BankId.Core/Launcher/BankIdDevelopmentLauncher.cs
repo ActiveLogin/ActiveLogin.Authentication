50c7226cafc939996daf9ef75b334d0551383347
Elin Fokine <ElinO@activesolution.se>
2025-08-29 16:47:43 +0200

    Use new constructor for BankIdLaunchInfo.

--- src/ActiveLogin.Authentication.BankId.Core/Launcher/BankIdDevelopmentLauncher.cs
+++ src/ActiveLogin.Authentication.BankId.Core/Launcher/BankIdDevelopmentLauncher.cs
@@ -3,10 +3,11 @@ namespace ActiveLogin.Authentication.BankId.Core.Launcher;
 internal class BankIdDevelopmentLauncher : IBankIdLauncher
 {
     private const string DevelopmentLaunchUrl = "#";
+    private const string DevelopmentReturnUrl = "";
 
     public Task<BankIdLaunchInfo> GetLaunchInfoAsync(LaunchUrlRequest request)
     {
         // Always stay on same page, without reloading, in simulated mode
-        return Task.FromResult(new BankIdLaunchInfo(DevelopmentLaunchUrl, false, false));
+        return Task.FromResult(new BankIdLaunchInfo(DevelopmentLaunchUrl, false, false, DevelopmentReturnUrl));
     }
 }
