b0968c0ce4b946c6600665a22a36d2691c6051a8
Elin Fokine <elin.fokine@activesolution.se>
2026-09-24 11:05:28 +0200

    Try out using return url for edge on Android.

--- src/ActiveLogin.Authentication.BankId.Core/Launcher/BankIdLauncher.cs
+++ src/ActiveLogin.Authentication.BankId.Core/Launcher/BankIdLauncher.cs
@@ -78,11 +78,20 @@ internal class BankIdLauncher : IBankIdLauncher
             BrowserReloadBehaviourOnReturnFromBankIdApp.Always => true,
             BrowserReloadBehaviourOnReturnFromBankIdApp.Never => false,
 
-            // By default, Safari on iOS will refresh the page/tab when returned from the BankID app
+            // By default, Safari on iOS will refresh the page/tab when returned from the BankID app.
+            //
+            // Edge on Android is given an explicit redirect/return URL (see GetRedirectUrl), since it
+            // does not reliably return to the calling tab/app when redirect=null is used. This means
+            // the page will also be reloaded for Edge on Android, just like Safari on iOS.
             _ => detectedDevice is
             {
                 DeviceOs: BankIdSupportedDeviceOs.Ios,
                 DeviceBrowser: BankIdSupportedDeviceBrowser.Safari
+            }
+            || detectedDevice is
+            {
+                DeviceOs: BankIdSupportedDeviceOs.Android,
+                DeviceBrowser: BankIdSupportedDeviceBrowser.Edge
             }
         };
     }
@@ -150,6 +159,14 @@ internal class BankIdLauncher : IBankIdLauncher
         if (customBrowserConfig != null && customBrowserConfig.ReturnUrl != null)
         {
             return customBrowserConfig.ReturnUrl;
+        }
+
+        // Edge on Android is an exception to the Android default (redirect=null): unlike other
+        // Android browsers, it does not reliably return to the calling tab/app otherwise, so it
+        // needs to be given the actual return URL to navigate back to.
+        if (device is { DeviceOs: BankIdSupportedDeviceOs.Android, DeviceBrowser: BankIdSupportedDeviceBrowser.Edge })
+        {
+            return request.RedirectUrl;
         }
 
         // Only use redirect url for iOS as recommended in BankID Guidelines 3.1.2
